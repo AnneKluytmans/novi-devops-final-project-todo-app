d02b513886acf2b2cbac7549a48374d9fda410a7
AnneKluytmans <annekluytmans1@gmail.com>
2025-12-26 18:06:40 +0100

    feat: add UI to create new todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,38 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+
+  const fetchTodos = () => {
+    axios.get('/api/todos').then(res => setTodos(res.data));
+  };
 
   useEffect(() => {
-    axios.get('/api/todos').then(res => setTodos(res.data));
+    fetchTodos();
   }, []);
+
+  const addTodo = async () => {
+    if (!title) return;
+
+    await axios.post('/api/todos', { title });
+    setTitle('');
+    fetchTodos();
+  };
 
   return (
     <div className="container">
       <h1>Todo App</h1>
 
+      <div className="add-todo">
+        <input
+          type="text"
+          placeholder="New todo..."
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      
       <ul>
         {todos.map(t => (
           <li key={t.id}>
