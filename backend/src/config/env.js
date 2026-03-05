70497364549fc8e360f0504e2107edd95669d389
AnneKluytmans <annekluytmans1@gmail.com>
2026-03-05 15:07:57 +0100

    fix: add CI condition in env config

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-const envFile =
-  process.env.NODE_ENV === 'test'
+const isCI = process.env.CI === 'true';
+
+if (isCI) {
+    const envFile =
+    process.env.NODE_ENV === 'test'
     ? '.env.test'
     : '.env';
+    
+    dotenv.config({
+        path: path.resolve(process.cwd(), envFile),
+    });
+}
 
-dotenv.config({
-  path: path.resolve(process.cwd(), envFile),
-});
